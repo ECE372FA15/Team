5e85590118453e2b27723f9836198cfa433c9a11
packagoustomifuw <simpspr@email.arizona.edu>
2015-10-06 15:14:06 -0700

    Added EX and IN switch states, initialized RD6 switch

--- lab1/lab1p3.X/main.c
+++ lab1/lab1p3.X/main.c
@@ -27,145 +27,158 @@
 #define mainFunction
 // state machine definitions 
 #ifdef mainFunction
-typedef enum stateTypeEnum{
-    dBPress, waitRelease, dBRelease, stoppedWaitForPress, runningWaitForPress
+
+typedef enum stateTypeEnum {
+    dBPressEX, dBPressIN, waitRelease, dBReleaseEX, dBReleaseIN, stoppedWaitForPress, runningWaitForPress
 } stateType;
 
 //global varables declared to work in ISRs 
-volatile stateType state = runningWaitForPress; 
-volatile uint64_t timeCountInHundredthsOfASecond = 0; 
+volatile stateType state = runningWaitForPress;
+volatile uint64_t timeCountInHundredthsOfASecond = 0;
 unsigned int dummyVariable = 0;
 
-
-int main(void)
-{
+int main(void) {
     ANSELE = 0;
     //Initialize new interrupt fix
     SYSTEMConfigPerformance(40000000);
 
-   // initialize the project components 
-   initSW(); 
-   initLCD();
-   clearLCD();
-   writeLCD(0b00001111, 0, 50);
-   initLED(runLED);
-   initLED(stopLED);
-   initTimer2();
-   enableInterrupts();
-   turnOffLED(stopLED);
-   turnOnLED(runLED);
-   initT1();
-   
-   // infinite loop 
-    while(1){
-        
-        switch(state){
-            // the state that toggles the LEDs 
+    // initialize the project components 
+    initSW();
+    initLCD();
+    clearLCD();
+    writeLCD(0b00001111, 0, 50);
+    initLED(runLED);
+    initLED(stopLED);
+    initTimer2();
+    enableInterrupts();
+    turnOffLED(stopLED);
+    turnOnLED(runLED);
+    initT1();
+
+    // infinite loop 
+    while (1) {
+
+        switch (state) {
+                // the state that toggles the LEDs 
             case stoppedWaitForPress:
                 turnOnLED(runLED);
                 turnOffLED(stopLED);
-                while(state == stoppedWaitForPress){
-                    if(PORTDbits.RD6 == PRESSED){     //CN Flag is high should be the condition?
+                while (state == stoppedWaitForPress) {
+                    if (PORTDbits.RD6 == PRESSED) { //CN Flag is high should be the condition?
                         timeCountInHundredthsOfASecond = 0; //should this be done in the ISR?
                     }
                 }
                 break;
-                
-            // wait for user input i.e. button press
+
+                // wait for user input i.e. button press
             case runningWaitForPress:
                 turnOnLED(stopLED);
                 turnOffLED(runLED);
                 while (state == runningWaitForPress);
                 break;
-                
-            // once the button has been pressed 
-            case dBPress:
-                
+
+                // once the button has been pressed 
+            case dBPressEX:
+
                 delayUs(DBdelayTime); // Delay for 5ms
-              
-                while(state == dBPress );
-               
+
+                while (state == dBPressEX);
+
                 break;
-            
-            // once the button has been released 
-            case dBRelease:
-                 
+                
+            case dBPressIN:
+                
+                break;
+
+                // once the button has been released 
+            case dBReleaseEX:
+
                 delayUs(DBdelayTime); //Delay for 5ms
 
-                while(state == dBRelease);
-                 
+                while (state == dBReleaseEX);
+
                 break;
-        } 
+                
+            case dBReleaseIN:
+                
+                
+                
+                break;
+        }
     }
 
     return 0;
 }
 
-void __ISR(_CHANGE_NOTICE_VECTOR, IPL6SRS) _CNInterrupt(void){
+void __ISR(_CHANGE_NOTICE_VECTOR, IPL6SRS) _CNInterrupt(void) {
 
-    dummyVariable = PORTAbits.RA7 = 1;//Put the CN flag down
-    IFS1bits.CNAIF = 0;
+    dummyVariable = PORTAbits.RA7 = 1; //Put the CN flag down
+    dummyVariable = PORTDbits.RD7 = 1;
+    
 
-   if(state == dBRelease){
-        // if the run led is on 
-        if(TRISGbits.TRISG12 == 1){
-            state = stoppedWaitForPress; // goto stopped 
-        }
-        else{
-            state = runningWaitForPress; // advance to go 
-        }
-    }
-    else if(state == dBPress){               
-        //Going from dbPress to dbRelease
-        state = dBRelease; 
+    if (IFS1bits.CNDIF == 1 && state == stoppedWaitForPress) {
+
     }
     else {
-        state = dBPress;
+        IFS1bits.CNAIF = 0;
+        if (state == dBReleaseEX) {
+            // if the run led is on 
+            if (TRISGbits.TRISG12 == 1) {
+                state = stoppedWaitForPress; // goto stopped 
+            } else {
+                state = runningWaitForPress; // advance to go 
+            }
+        } else if (state == dBPressEX) {
+            //Going from dbPress to dbRelease
+            state = dBReleaseEX;
+        } else {
+            state = dBPressEX;
+        }
     }
 }
 //time timer that should go off every 1/100 th of a second 
-void __ISR(_TIMER_1_VECTOR, IPL7SRS) _T1Interrupt(void){
-    
+
+void __ISR(_TIMER_1_VECTOR, IPL7SRS) _T1Interrupt(void) {
+
     // set the flag down 
     IFS0bits.T1IF = 0;
-   // dummyVariable = PORTAbits.RA7 = 1;//Put the CN flag down
-    
-      // if the run led is on 
-       if(state == runningWaitForPress){
-            // increment the timer 
-            timeCountInHundredthsOfASecond ++ ;
-            // display running message and time 
-           writeRunning(timeCountInHundredthsOfASecond); 
-       }
-       else{
-           // display stopped message and time 
-           writeStopped(timeCountInHundredthsOfASecond);
-       }
+    // dummyVariable = PORTAbits.RA7 = 1;//Put the CN flag down
+
+    // if the run led is on 
+    if (state == runningWaitForPress) {
+        // increment the timer 
+        timeCountInHundredthsOfASecond++;
+        // display running message and time 
+        writeRunning(timeCountInHundredthsOfASecond);
+    } else {
+        // display stopped message and time 
+        writeStopped(timeCountInHundredthsOfASecond);
+    }
 }
 
 #endif
 
 #ifdef test
 
-int main(void){
-ANSELE = 0;
+int main(void) {
+    ANSELE = 0;
     //Initialize new interrupt fix
     SYSTEMConfigPerformance(40000000);
-    
-   // initialize the project components 
-   initSW(); 
-   
-   initLCD();
-   clearLCD();
-   writeLCD(0b00001111, 0, 50);
-   //initLED(runLED);
-   //initLED(stopLED);
-   initTimer2();
-   enableInterrupts();
-   //turnOffLED(stopLED);
-   //turnOnLED(runLED);
-   testPrintTimeLCD();
-   
+
+    // initialize the project components 
+    initSW();
+
+    initLCD();
+    clearLCD();
+    writeLCD(0b00001111, 0, 50);
+    //initLED(runLED);
+    //initLED(stopLED);
+    initTimer2();
+    enableInterrupts();
+    //turnOffLED(stopLED);
+    //turnOnLED(runLED);
+    testPrintTimeLCD();
+
 }
 
 #endif
--- lab1/lab1p3.X/switch.c
+++ lab1/lab1p3.X/switch.c
@@ -16,7 +16,7 @@
 
 void initSW(){
     //TODO: Initialize the appropriate pins to work with the external switch using a
-    //change notification interrupt.
+    //change notification interrupts
     TRISAbits.TRISA7 = INPUT;   // Enable input for switch
     CNCONAbits.ON = 1;          // Turn on CN device
     CNENAbits.CNIEA7 = 1;       // Enable CN interrupt for pin
@@ -24,6 +24,17 @@ void initSW(){
     IFS1bits.CNAIF = 0;         // Put Interrupt flag down
     IPC8bits.CNIP = 6;
     IEC1bits.CNAIE = 1;         // Enable overall CN Interrupt
+    
+    
+    //Initialize the onboard switch
+   TRISDbits.TRISD6 = INPUT;   // Enable input for switch
+    CNCONDbits.ON = 1;          // Turn on CN device
+    CNENDbits.CNIED6 = 1;       // Enable CN interrupt for pin
+    CNPUDbits.CNPUD6 = 1;       // Enable Internal Pull-up resistor
+    IFS1bits.CNDIF = 0;         // Put Interrupt flag down
+    IPC8bits.CNIP = 6;
+    IEC1bits.CNDIE = 1;         // Enable overall CN Interrupt
+    
 }
 
 
