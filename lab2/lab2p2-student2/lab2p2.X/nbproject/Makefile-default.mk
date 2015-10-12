a653434d28fbf3f736246230597194b2afef724f
abigailfrancis <abigailfrancis@email.arizona.edu>
2015-10-12 14:38:42 -0700

    checkValid()

--- lab2/lab2p2-student2/lab2p2.X/nbproject/Makefile-default.mk
+++ lab2/lab2p2-student2/lab2p2.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=interrupt.c keypad.c main.c lcd.c timer.c password.c
+SOURCEFILES_QUOTED_IF_SPACED=interrupt.c keypad.c main.c lcd.c timer.c password.c passcode.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/interrupt.o ${OBJECTDIR}/keypad.o ${OBJECTDIR}/main.o ${OBJECTDIR}/lcd.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/password.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/interrupt.o.d ${OBJECTDIR}/keypad.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/lcd.o.d ${OBJECTDIR}/timer.o.d ${OBJECTDIR}/password.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/interrupt.o ${OBJECTDIR}/keypad.o ${OBJECTDIR}/main.o ${OBJECTDIR}/lcd.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/password.o ${OBJECTDIR}/passcode.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/interrupt.o.d ${OBJECTDIR}/keypad.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/lcd.o.d ${OBJECTDIR}/timer.o.d ${OBJECTDIR}/password.o.d ${OBJECTDIR}/passcode.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/interrupt.o ${OBJECTDIR}/keypad.o ${OBJECTDIR}/main.o ${OBJECTDIR}/lcd.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/password.o
+OBJECTFILES=${OBJECTDIR}/interrupt.o ${OBJECTDIR}/keypad.o ${OBJECTDIR}/main.o ${OBJECTDIR}/lcd.o ${OBJECTDIR}/timer.o ${OBJECTDIR}/password.o ${OBJECTDIR}/passcode.o
 
 # Source Files
-SOURCEFILES=interrupt.c keypad.c main.c lcd.c timer.c password.c
+SOURCEFILES=interrupt.c keypad.c main.c lcd.c timer.c password.c passcode.c
 
 
 CFLAGS=
@@ -130,6 +130,12 @@ ${OBJECTDIR}/password.o: password.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/password.o 
 	@${FIXDEPS} "${OBJECTDIR}/password.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/password.o.d" -o ${OBJECTDIR}/password.o password.c   
 	
+${OBJECTDIR}/passcode.o: passcode.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/passcode.o.d 
+	@${RM} ${OBJECTDIR}/passcode.o 
+	@${FIXDEPS} "${OBJECTDIR}/passcode.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/passcode.o.d" -o ${OBJECTDIR}/passcode.o passcode.c   
+	
 else
 ${OBJECTDIR}/interrupt.o: interrupt.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -166,6 +172,12 @@ ${OBJECTDIR}/password.o: password.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/password.o.d 
 	@${RM} ${OBJECTDIR}/password.o 
 	@${FIXDEPS} "${OBJECTDIR}/password.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/password.o.d" -o ${OBJECTDIR}/password.o password.c   
+	
+${OBJECTDIR}/passcode.o: passcode.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/passcode.o.d 
+	@${RM} ${OBJECTDIR}/passcode.o 
+	@${FIXDEPS} "${OBJECTDIR}/passcode.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/passcode.o.d" -o ${OBJECTDIR}/passcode.o passcode.c   
 	
 endif
 
--- lab2/lab2p2-student2/lab2p2.X/passcode.c
+++ lab2/lab2p2-student2/lab2p2.X/passcode.c
@@ -1,8 +1,18 @@
 #include "passcode.h"
-
-int checkValid(char * str){
-
-    return 0;
+#include <string.h>
+//Compares entered password with stored passwords
+int checkValid(char * str, char * passWord[][]){
+    int i = 0;
+    int match = 0;
+    int temp = 0;
+    for (i = 0; i < 10; i++){
+        temp = strcmp(str, passWord[i]);    //returns 0 for match 
+        if (temp == 0){
+            match = 1;                      //match becomes 1 for any match
+        }
+    }
+    //return 1 for valid, return 0 for not valid
+    return match;
 }
 
 // checks a string to make sure it is a valid pw entry 
