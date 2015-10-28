f480e32fb06c6635e493759fcf5cd97549ba3c3b
jonathanghawkins <jonathanghawkins@email.arizona.edu>
2015-10-28 12:37:42 -0700

    kkskkdfks

---
+++ lab3/lab3p1.X/motor.h
@@ -0,0 +1,18 @@
+
+
+
+void initRightMotor(); 
+void initLeftMotor();
+
+void leftMotorForward(int speed);
+void leftMotorBackward(int speed);
+void leftMotorStop();
+
+void rightMotorForward(int speed);
+void rightMotorBackward(int speed);
+void rightMotorStop();
+
+
+
+
+
