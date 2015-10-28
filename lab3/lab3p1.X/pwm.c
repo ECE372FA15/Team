2ca17784c6d597010e018efe2fc874bea902d1c3
Brandon-Lipjanic <blipjanic1616@email.arizona.edu>
2015-10-28 13:28:46 -0700

    Set up inits for the OC1-4

--- lab3/lab3p1.X/pwm.c
+++ lab3/lab3p1.X/pwm.c
@@ -14,15 +14,27 @@ void initPWM(){
     OC2R = 0x0000; // Initialize primary Compare Register
     OC2RS = 0x0000; // Initialize secondary Compare Register
     OC2CON = 0x0006; // Configure for PWM mode
-    OC2CONSET = 0x8000; // Enable OC1
+    OC2CONSET = 0x8000; // Enable OC2
 
    //!!!!!!!!!!!! is the next line right? idk... 
-    RPD1Rbits.RPD1R = 0b1011; // map OC2 to RD1
+    RPD0Rbits.RPD0R= 0b1100; // map OC1 to RD0
     OC1CON = 0x0000; // Turn off OC1 while doing setup.
     OC1R = 0x0000; // Initialize primary Compare Register
     OC1RS = 0x0000; // Initialize secondary Compare Register
     OC1CON = 0x0006; // Configure for PWM mode
     OC1CONSET = 0x8000; // Enable OC1
 
+    RPD2Rbits.RPD2R= 0b1011; // map OC3 to RD2
+    OC3CON = 0x0000; // Turn off OC1 while doing setup.
+    OC3R = 0x0000; // Initialize primary Compare Register
+    OC3RS = 0x0000; // Initialize secondary Compare Register
+    OC3CON = 0x0006; // Configure for PWM mode
+    OC3CONSET = 0x8000; // Enable OC1
 
+    RPD3Rbits.RPD3R= 0b1011; // map OC4 to RD3
+    OC1CON = 0x0000; // Turn off OC1 while doing setup.
+    OC1R = 0x0000; // Initialize primary Compare Register
+    OC1RS = 0x0000; // Initialize secondary Compare Register
+    OC1CON = 0x0006; // Configure for PWM mode
+    OC1CONSET = 0x8000; // Enable OC1
 }
